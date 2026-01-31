8091b1e1be7e4f2b20593c988a5015b7a760bfe4
anishghanwat <anishghanwat.work@gmail.com>
2026-02-01 03:08:10 +0530

    Clean up package.json: remove backend dependencies for serverless approach

--- index.js
+++
@@ -1,8 +0,0 @@
-// This is a root-level entry point for Railway deployment
-// The actual backend code is in the /backend directory
-
-console.log('StoreMyBottle - Please deploy from /backend directory');
-console.log('This root file is just for Railway detection');
-
-// Redirect to backend if someone tries to run this
-process.exit(1);
